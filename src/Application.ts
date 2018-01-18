aed5e57321e2299f9a2187c4edf7877600bd2af2
Johannes Diemke <johannes.diemke@eventim.de>
2018-01-18 23:24:53 +0100

    Basic sound generation prototype.

--- src/Application.ts
+++ src/Application.ts
@@ -1,7 +1,21 @@
 class Application {
 
     public main(): void {
-        console.log('Hello world!');
+        const audioContext: AudioContext = new AudioContext();
+        const oscillator: OscillatorNode = audioContext.createOscillator();
+        const bufferSize: number = 4096;
+        const scriptProcessorNode: ScriptProcessorNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+        const FREQUENCY: number = 440;
+        let currentPhase: number = 0.0;
+        const phaseIncrement: number = 2 * Math.PI * FREQUENCY / audioContext.sampleRate;
+        scriptProcessorNode.onaudioprocess = (event: AudioProcessingEvent): void => {
+            const output: Float32Array = event.outputBuffer.getChannelData(0);
+            for (let i: number = 0; i < bufferSize; i++) {
+                output[i] = Math.sin(currentPhase); // Math.random() * 2 - 1;
+                currentPhase += phaseIncrement;
+            }
+        };
+        scriptProcessorNode.connect(audioContext.destination);
     }
 
 }
